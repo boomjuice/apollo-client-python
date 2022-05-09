41d4c22dd8746374a1d30c26cff68c66a3f202b8
黄嘉文 <huangjiawen@belle.com.cn>
2022-05-09 11:57:52 +0800

    apollo-client-python初始化

--- src/apollo_client_python/logs.py
+++ src/apollo_client_python/logs.py
@@ -1,9 +1,3 @@
 import logging
 
-logging.basicConfig(
-    format=
-    '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-    datefmt='%d-%m-%Y:%H:%M:%S',
-    level=logging.DEBUG)
-
 logger = logging.getLogger('apollo')
